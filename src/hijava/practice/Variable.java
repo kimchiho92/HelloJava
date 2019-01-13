1be9c4e2c1e46315a0385bcb806e1c9c085172f4
dohyekim <dhleen5@hanmail.net>
2019-01-13 13:48:20 +0900

    z

---
+++ src/hijava/practice/Variable.java
@@ -0,0 +1,23 @@
+package hijava.practice;
+
+public class Variable {
+	public static void main(String[]args) {
+		int a= 12;
+		float b=  2.4f;
+		int r1=(int)(a+b);
+		int r2=(int)(a*b);
+		System.out.println(r1);
+		System.out.println(r2);
+		System.out.println("++++++++++++++++++++");
+		System.out.println(a+b);
+		System.out.println(a*b);
+		
+		
+		double d= 12.4;
+		int i= 3;
+		System.out.println("================================");
+		System.out.println(d%i);
+		System.out.println((int)d%i);
+		
+	}
+}
