5e78c07d086111965c989250f1de9bd386fb1aa2
dohyekim <dhleen5@hanmail.net>
2019-01-20 09:29:51 +0900

    ddd

--- src/hijava/practice2/Loop5.java
+++ src/hijava/practice2/Loop5.java
@@ -9,14 +9,25 @@ public class Loop5 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("높이  입력");
 		int line =sc.nextInt(); //i= 입력 받는수 세로 길이 
+for (int i = 1; i <= line; i++) {
 			
-		for(int i= 1;i<=line;i++) {
-			for(int j=1;j<(line-1);j++) {
-				System.out.println(' ');
+			for (int j = 1; j <= (line - i); j++) {
+				System.out.print('-');
 			}
-			for(int k=1; k<=(2*i-1);k++) {
+			
+			for (int j = 1; j <= (2 * i - 1); j++) {
 				System.out.print('*');
 			}
+			
+			System.out.println();
+	/*	for(int i= 1;i<=line;i++) {
+	 * 
+			for(int j = 1;j <(line-i);j++) {
+				System.out.print(' ');
+			}
+			for(int k=1; k<=(2*i-1);k++) {
+				System.out.println('*');
+			}*/
 		}
 		
 	}
---
+++ src/hijava/practice2/Loop6.java
@@ -0,0 +1,5 @@
+package hijava.practice2;
+
+public class Loop6 {
+
+}
